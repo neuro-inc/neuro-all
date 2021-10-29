0af56d8bcd5d722243e7725ff73173a09e8335e2
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-10-29 12:32:12 +0300

    Bump to 21.10.2

--- tools/update_changelog.py
+++ tools/update_changelog.py
@@ -5,9 +5,10 @@ import subprocess
 from all_repos.clone import main as clone_main
 from difflib import SequenceMatcher
 from pathlib import Path
+from typing import Optional
 
 
-def fetch(name: str) -> str:
+def fetch(name: str) -> Optional[str]:
     changelog = Path("cloned") / name / "CHANGELOG.md"
     txt = changelog.read_text()
     TEMPLATE = "[comment]: # (towncrier release notes start)\n"
@@ -17,6 +18,8 @@ def fetch(name: str) -> str:
     left = old.read_text()
     matcher = SequenceMatcher(None, left, right)
     for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+        if tag == "equal":
+            return None
         if tag != "insert":
             raise ValueError(
                 f"insert is expected as a first matcher opcode, "
@@ -26,12 +29,22 @@ def fetch(name: str) -> str:
         return right[j1:j2]
 
 
+UPSTREAMS = ("platform-client-python", "neuro-extras", "neuro-flow")
+
+
 @click.command()
 def main() -> None:
     clone_main()
     changes = []
-    for name in ("platform-client-python", "neuro-extras", "neuro-flow"):
-        changes.append(fetch(name))
+    for name in UPSTREAMS:
+        ret = fetch(name)
+        if ret is not None:
+            changes.append(ret)
+
+    if not changes:
+        raise click.UsageError(
+            "CHANGELOG.md is not updated:\n" f"Nothing changed in upstreams {UPSTREAMS}"
+        )
 
     changelog = Path("CHANGELOG.md")
     txt = changelog.read_text()
@@ -47,6 +60,8 @@ def main() -> None:
     header = f"Neuro {version} ({date})\n"
     header += "=" * (len(header) - 1) + "\n"
     changelog.write_text(pre + "\n\n" + header + "".join(changes) + post)
+
+    click.echo("CHANGELOG.md is updated")
 
 
 if __name__ == "__main__":
